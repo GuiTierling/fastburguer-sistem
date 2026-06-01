c4025cd95670cdfb7433a2147fcb929a99552f5a
Guilherme Campos Tierling <114.042@alunos.unigrancapital.com.br>
2026-06-01 13:23:21 -0400

    feat: Adicionar os dados do pedido do cliente - RF-01

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Digite o seu nome: ");
+string nome = Console.ReadLine();
+
+Console.Write("Digite a sua idade: ");
+int idade = int.TryParse(Console.ReadLine());
+
+if (idade >= 18)
+{
+    Console.Write($"Você é maior de idade!");
+} else
+{
+    Console.Write($"O {nome} ainda é de menor");
+}
